b547980939dccfbd5d9eae7fde0c4f29d25e11ac
Nmerkel <noramerkel0@gmail.com>
2022-11-10 15:16:39 -0500

    created project in baseEntity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,10 +2,7 @@ package com.cydeo.entity;
 
 import lombok.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -19,18 +16,25 @@ public class BaseEntity {
     private Long id;
 
     private boolean isDeleted = false;
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
-
+// the @PrePersist will run when creating an object(user) when using @Prepersist(when you try to save)
+    @PrePersist
     private void onPrePersist(){
         this.insertDateTime=LocalDateTime.now();
         this.lastUpdateDateTime=LocalDateTime.now();
         this.insertUserId=1L;
         this.lastUpdateUserId=1L;
     }
+    // this will be implemented when updating a user by using annotation @PreUpdate(when you try to update)
+    @PreUpdate
     private void onPreUpdate(){
         this.lastUpdateDateTime=LocalDateTime.now();
         this.lastUpdateUserId=1L;
