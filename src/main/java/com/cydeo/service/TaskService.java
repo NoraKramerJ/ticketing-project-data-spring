157d5b7f155038b16132ab87cae4acf7dd7107bd
Nmerkel <noramerkel0@gmail.com>
2022-11-12 19:30:28 -0500

    working on pending tasks methods in the task controller

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -5,6 +5,7 @@ package com.cydeo.service;
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -29,5 +30,11 @@ public interface TaskService {
     void deleteByProject(ProjectDTO projectDTO);
 
     void completeByProject(ProjectDTO projectDTO);
+
+    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+
+    List<TaskDTO> listAllTasksByStatus(Status status);
+
 }
+
 
